181d05c088128683ba96a3a604f4edecc3993b3a
Graysire <Graysire@dyrix.net>
2022-07-24 10:46:20 -0700

    Added Basic RPG Item dervied class of EntryData

---
+++ TreasureGenerator/TreasureGenerator/RPGData/Item.cpp
@@ -0,0 +1,69 @@
+#include "Item.h"
+#include <sstream>
+
+#define QUANTITYMULT (printEach ? 1 : quantity)
+
+bool Item::printEach = true;
+
+Item::Item() : EntryData("Generic Item", 1), value(0), weight(0)
+{
+
+}
+
+Item::Item(std::string name, unsigned long value, unsigned long weight) : EntryData(name, 1), value(value), weight(weight)
+{
+
+}
+
+Item::Item(std::string name, unsigned int quantity, unsigned long value, unsigned long weight) : EntryData(name, quantity), value(value), weight(weight)
+{
+
+}
+
+Item::Item(const Item& obj) : EntryData(obj), value(obj.value), weight(obj.weight)
+{
+}
+
+std::string Item::GetResultString(unsigned int quantity)
+{
+	return EntryData::GetResultString(quantity);
+}
+
+std::string Item::GetResultString()
+{
+	std::ostringstream result;
+	result << EntryData::GetResultString();
+
+	result.setf(std::ios::right);
+	result.width(FirstDescriptorWidth);
+	result << (QUANTITYMULT * value / 1000.0) << "sp ";
+	result.width(SecondDescriptorWidth);
+	result << (QUANTITYMULT * weight / 1000.0) << "lb";
+
+	return result.str();
+}
+
+unsigned long Item::GetValue()
+{
+	return value;
+}
+
+void Item::SetValue(unsigned long value)
+{
+	this->value = value;
+}
+
+unsigned long Item::GetWeight()
+{
+	return weight;
+}
+
+void Item::SetWeight(unsigned long weight)
+{
+	this->weight = weight;
+}
+
+void Item::SetPrintEach(bool printEach)
+{
+	Item::printEach = printEach;
+}
---
+++ TreasureGenerator/TreasureGenerator/RPGData/Item.h
@@ -0,0 +1,31 @@
+#ifndef Item_H
+#define Item_H
+
+#include "../DataGenerators/EntryData.h"
+
+class Item : public EntryData
+{
+public:
+	Item();
+	Item(std::string name, unsigned long value, unsigned long weight);
+	Item(std::string name, unsigned int quantity, unsigned long value, unsigned long weight);
+	Item(const Item& obj);
+
+	virtual std::string GetResultString(unsigned int quantity);
+	virtual std::string GetResultString();
+
+	unsigned long GetValue();
+	void SetValue(unsigned long value);
+	unsigned long GetWeight();
+	void SetWeight(unsigned long weight);
+	static void SetPrintEach(bool printEach);
+protected:
+	//Value in 1000ths of a unit
+	unsigned long value;
+	//Weight ib 1000ths of a unit
+	unsigned long weight;
+	//Print per quantity or total
+	static bool printEach;
+};
+
+#endif // !Item_H
